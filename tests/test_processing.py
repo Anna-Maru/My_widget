81c6e0444e2cf94acacb45bc6542179a850bddf5
Anna <annamarugina123@gmail.com>
2025-07-25 15:02:49 +0300

    revision

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,16 +1,26 @@
-from my_widget.processing import filter_by_state
+import pytest
+from src.processing import filter_by_state
 
-records = [
+
+def test_filter_by_state_default():
+    records = [
     {"id": 41428829, "state": "EXECUTED", "date": "..."},
     {"id": 594226727, "state": "CANCELED", "date": "..."},
     {"id": 939719570, "state": "EXECUTED", "date": "..."},
-]
+    ]
 
-assert filter_by_state(records) == [
-    {"id": 41428829, "state": "EXECUTED", "date": "..."},
-    {"id": 939719570, "state": "EXECUTED", "date": "..."},
-]
+    assert filter_by_state(records) == [
+        {"id": 41428829, "state": "EXECUTED", "date": "..."},
+        {"id": 939719570, "state": "EXECUTED", "date": "..."},
+    ]
 
-assert filter_by_state(records, state="CANCELED") == [
+def test_filter_by_state_canceled():
+    records = [
+        {"id": 41428829, "state": "EXECUTED", "date": "..."},
+        {"id": 594226727, "state": "CANCELED", "date": "..."},
+        {"id": 939719570, "state": "EXECUTED", "date": "..."},
+    ]
+
+    assert filter_by_state(records, state="CANCELED") == [
     {"id": 594226727, "state": "CANCELED", "date": "..."}
-]
+    ]
